24e6588a400fd5b30157d9e0aec8d40f8219ad39
m-ratcliffe <158192795+m-ratcliffe@users.noreply.github.com>
2024-09-26 12:00:05 +0200

    More Optimization

--- project-1-main/Project/source/module2.py
+++ project-1-main/Project/source/module2.py
@@ -7,7 +7,7 @@ configuration = {
     "portVar" : "COM4",
     "blurIncrement": 2,
     "initialDir" : None,
-    "defaultImg" : None,
+    "defaultImg" : "assets/default.jpg",
     "hole" : ["A1"],#, "A2", "A3", "A4", "B1", "B2", "B3", "B4", "C1", "C2", "C3", "C4", "D1", "D2", "D3", "D4"],
     "stick" : ["Red"]#, "Green", "Blue", "Yellow"]
 }
