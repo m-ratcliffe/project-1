a36036a0aaae5f27effef491c09418627f2db103
m-ratcliffe <158192795+m-ratcliffe@users.noreply.github.com>
2024-09-26 12:05:15 +0200

    -

--- project-1-main/Project/source/module2.py
+++ project-1-main/Project/source/module2.py
@@ -6,7 +6,7 @@ import os, sys
 configuration = {
     "portVar" : "COM4",
     "blurIncrement": 2,
-    "initialDir" : None,
+    "initialDir" : "assets",
     "defaultImg" : "assets/default.jpg",
     "hole" : ["A1"],#, "A2", "A3", "A4", "B1", "B2", "B3", "B4", "C1", "C2", "C3", "C4", "D1", "D2", "D3", "D4"],
     "stick" : ["Red"]#, "Green", "Blue", "Yellow"]
